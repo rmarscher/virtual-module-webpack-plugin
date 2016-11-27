9d36436fe83f7e51ad7b2495fe3360c86149270d
nathb2b@gmail.com <nathb2b@gmail.com>
2016-11-27 10:48:24 +0100

    ^ node 6.x compatibility

--- virtual-stats.js
+++ virtual-stats.js
@@ -8,88 +8,95 @@
  */
 
 'use strict';
-
-const constants = process.binding('constants');
+/**
+ * On node 6.x the fs constants are located in process.binding('constants').fs...
+ */
+const constants      =
+          process.versions.node.split('.')[0] > 6 ?// not tested on 5.x
+          process.binding('constants').fs
+              :
+          process.binding('constants');
 
 class VirtualStats {
-  /**
-   * Create a new stats object.
-   * @param {Object} config Stats properties.
-   * @constructor
-   */
-  constructor(config) {
-    for (const key in config) {
-      if (!config.hasOwnProperty(key)) {
-        continue;
-      }
-      this[key] = config[key];
+    /**
+     * Create a new stats object.
+     * @param {Object} config Stats properties.
+     * @constructor
+     */
+    constructor( config ) {
+        for ( const key in config ) {
+            if ( !config.hasOwnProperty(key) ) {
+                continue;
+            }
+            this[key] = config[key];
+        }
     }
-  }
 
-  /**
-   * Check if mode indicates property.
-   * @param {number} property Property to check.
-   * @return {boolean} Property matches mode.
-   */
-  _checkModeProperty(property) {
-    return ((this.mode & constants.S_IFMT) === property);
-  }
-
-
-  /**
-   * @return {Boolean} Is a directory.
-   */
-  isDirectory() {
-    return this._checkModeProperty(constants.S_IFDIR);
-  }
+    /**
+     * Check if mode indicates property.
+     * @param {number} property Property to check.
+     * @return {boolean} Property matches mode.
+     */
+    _checkModeProperty( property ) {
+        return ((this.mode & constants.S_IFMT) === property);
+    }
 
 
-  /**
-   * @return {Boolean} Is a regular file.
-   */
-  isFile() {
-    return this._checkModeProperty(constants.S_IFREG);
-  }
+    /**
+     * @return {Boolean} Is a directory.
+     */
+    isDirectory() {
+        return this._checkModeProperty(constants.S_IFDIR);
+    }
 
 
-  /**
-   * @return {Boolean} Is a block device.
-   */
-  isBlockDevice() {
-    return this._checkModeProperty(constants.S_IFBLK);
-  }
+    /**
+     * @return {Boolean} Is a regular file.
+     */
+    isFile() {
+
+        return this._checkModeProperty(constants.S_IFREG);
+    }
 
 
-  /**
-   * @return {Boolean} Is a character device.
-   */
-  isCharacterDevice() {
-    return this._checkModeProperty(constants.S_IFCHR);
-  }
+    /**
+     * @return {Boolean} Is a block device.
+     */
+    isBlockDevice() {
+        return this._checkModeProperty(constants.S_IFBLK);
+    }
 
 
-  /**
-   * @return {Boolean} Is a symbolic link.
-   */
-  isSymbolicLink() {
-    return this._checkModeProperty(constants.S_IFLNK);
-  }
+    /**
+     * @return {Boolean} Is a character device.
+     */
+    isCharacterDevice() {
+        return this._checkModeProperty(constants.S_IFCHR);
+    }
 
 
-  /**
-   * @return {Boolean} Is a named pipe.
-   */
-  isFIFO() {
-    return this._checkModeProperty(constants.S_IFIFO);
-  }
+    /**
+     * @return {Boolean} Is a symbolic link.
+     */
+    isSymbolicLink() {
+        return this._checkModeProperty(constants.S_IFLNK);
+    }
 
 
-  /**
-   * @return {Boolean} Is a socket.
-   */
-  isSocket() {
-    return this._checkModeProperty(constants.S_IFSOCK);
-  }
+    /**
+     * @return {Boolean} Is a named pipe.
+     */
+    isFIFO() {
+        return this._checkModeProperty(constants.S_IFIFO);
+    }
+
+
+    /**
+     * @return {Boolean} Is a socket.
+     */
+    isSocket() {
+        return this._checkModeProperty(constants.S_IFSOCK);
+    }
 }
 
 module.exports = VirtualStats;

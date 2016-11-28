5c6da627158db3eba503f930dad9e2e30c222a68
Rob Marscher <rob@robmarscher.com>
2016-11-28 15:56:43 -0500

    Require constants instead of using process.binding
    This seems to work even in Node 7.x.

--- virtual-stats.js
+++ virtual-stats.js
@@ -7,100 +7,92 @@
  * @link https://github.com/tschaub/mock-fs/blob/master/license.md
  */
 
+/* eslint-disable no-restricted-syntax, no-prototype-builtins, no-continue */
+/* eslint-disable no-bitwise, no-underscore-dangle */
+
 'use strict';
 
-/**
- * On node 6.x the fs constants are located in process.binding('constants').fs...
- */
-let constants = process.binding('constants');
-constants     = constants.fs || constants;
-
-// const constants      =
-//           process.versions.node.split('.')[0] > 6 ?// not tested on 5.x
-//           process.binding('constants').fs
-//               :
-//           process.binding('constants');
+const constants = require('constants');
 
 class VirtualStats {
-    /**
-     * Create a new stats object.
-     * @param {Object} config Stats properties.
-     * @constructor
-     */
-    constructor( config ) {
-        for ( const key in config ) {
-            if ( !config.hasOwnProperty(key) ) {
-                continue;
-            }
-            this[key] = config[key];
-        }
+  /**
+   * Create a new stats object.
+   * @param {Object} config Stats properties.
+   * @constructor
+   */
+  constructor(config) {
+    for (const key in config) {
+      if (!config.hasOwnProperty(key)) {
+        continue;
+      }
+      this[key] = config[key];
     }
+  }
 
-    /**
-     * Check if mode indicates property.
-     * @param {number} property Property to check.
-     * @return {boolean} Property matches mode.
-     */
-    _checkModeProperty( property ) {
-        return ((this.mode & constants.S_IFMT) === property);
-    }
-
-
-    /**
-     * @return {Boolean} Is a directory.
-     */
-    isDirectory() {
-        return this._checkModeProperty(constants.S_IFDIR);
-    }
+  /**
+   * Check if mode indicates property.
+   * @param {number} property Property to check.
+   * @return {boolean} Property matches mode.
+   */
+  _checkModeProperty(property) {
+    return ((this.mode & constants.S_IFMT) === property);
+  }
 
 
-    /**
-     * @return {Boolean} Is a regular file.
-     */
-    isFile() {
-
-        return this._checkModeProperty(constants.S_IFREG);
-    }
-
-
-    /**
-     * @return {Boolean} Is a block device.
-     */
-    isBlockDevice() {
-        return this._checkModeProperty(constants.S_IFBLK);
-    }
+  /**
+   * @return {Boolean} Is a directory.
+   */
+  isDirectory() {
+    return this._checkModeProperty(constants.S_IFDIR);
+  }
 
 
-    /**
-     * @return {Boolean} Is a character device.
-     */
-    isCharacterDevice() {
-        return this._checkModeProperty(constants.S_IFCHR);
-    }
+  /**
+   * @return {Boolean} Is a regular file.
+   */
+  isFile() {
+    return this._checkModeProperty(constants.S_IFREG);
+  }
 
 
-    /**
-     * @return {Boolean} Is a symbolic link.
-     */
-    isSymbolicLink() {
-        return this._checkModeProperty(constants.S_IFLNK);
-    }
+  /**
+   * @return {Boolean} Is a block device.
+   */
+  isBlockDevice() {
+    return this._checkModeProperty(constants.S_IFBLK);
+  }
 
 
-    /**
-     * @return {Boolean} Is a named pipe.
-     */
-    isFIFO() {
-        return this._checkModeProperty(constants.S_IFIFO);
-    }
+  /**
+   * @return {Boolean} Is a character device.
+   */
+  isCharacterDevice() {
+    return this._checkModeProperty(constants.S_IFCHR);
+  }
 
 
-    /**
-     * @return {Boolean} Is a socket.
-     */
-    isSocket() {
-        return this._checkModeProperty(constants.S_IFSOCK);
-    }
+  /**
+   * @return {Boolean} Is a symbolic link.
+   */
+  isSymbolicLink() {
+    return this._checkModeProperty(constants.S_IFLNK);
+  }
+
+
+  /**
+   * @return {Boolean} Is a named pipe.
+   */
+  isFIFO() {
+    return this._checkModeProperty(constants.S_IFIFO);
+  }
+
+
+  /**
+   * @return {Boolean} Is a socket.
+   */
+  isSocket() {
+    return this._checkModeProperty(constants.S_IFSOCK);
+  }
 }
 
 module.exports = VirtualStats;
